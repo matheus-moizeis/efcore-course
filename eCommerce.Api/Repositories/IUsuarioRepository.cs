e0d512a375932fee15c0a1c09cc10f1bf1509e96
Matheus Moizeis <matheusmoizeis@outlook.com>
2023-08-31 23:15:23 -0300

    Adicionado repository do usuario

---
+++ eCommerce.Api/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using eCommerce.Models;
+
+namespace eCommerce.Api.Repositories
+{
+    public interface IUsuarioRepository
+    {
+        List<Usuario> Get();
+        Usuario Get(int id);
+        void Add(Usuario usuario);
+        void Update(Usuario usuario);
+        void Delete(int id);
+    }
+}
