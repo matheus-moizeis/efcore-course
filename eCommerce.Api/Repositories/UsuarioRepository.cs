bfe0ad0244812eabee2fe33380db6b223c9117ff
Matheus Moizeis <matheusmoizeis@outlook.com>
2023-09-21 23:46:53 -0300

    Ajustando repository de usuario para salvar no bd.

--- eCommerce.Api/Repositories/UsuarioRepository.cs
+++ eCommerce.Api/Repositories/UsuarioRepository.cs
@@ -1,33 +1,40 @@
-﻿using eCommerce.Models;
+﻿using eCommerce.Api.Database;
 
 namespace eCommerce.Api.Repositories
 {
     public class UsuarioRepository : IUsuarioRepository
     {
-        public static List<Usuario> _db = new List<Usuario>();
+        private readonly eCommerceContext _db;
+        public UsuarioRepository(eCommerceContext db)
+        {
+            _db = db;
+        }
+
         public List<Usuario> Get()
         {
-            return _db;
+            return _db.Usuarios.OrderBy(u => u.Id).ToList();
         }
 
         public Usuario Get(int id)
         {
-            return _db.Find(x => x.Id == id)!;
+            return _db.Usuarios.Find(id)!;
         }
 
         public void Add(Usuario usuario)
         {
-            _db.Add(usuario);
+            _db.Usuarios.Add(usuario);
+            _db.SaveChanges();
         }
         public void Update(Usuario usuario)
         {
-            _db.Remove(Get(usuario.Id));
-            _db.Add(usuario);
+            _db.Usuarios.Update(usuario);
+            _db.SaveChanges();
         }
 
         public void Delete(int id)
         {
-            _db.Remove(Get(id));
+            _db.Usuarios.Remove(Get(id));
+            _db.SaveChanges();
         }
 
     }
