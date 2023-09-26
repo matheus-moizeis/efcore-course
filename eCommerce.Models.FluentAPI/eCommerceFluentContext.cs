d8263053c1eda240c272a661cfa100a4585254c4
Matheus Moizeis <matheusmoizeis@outlook.com>
2023-09-25 23:38:38 -0300

    Criação e configuração inicial do FluentApi.

---
+++ eCommerce.Models.FluentAPI/eCommerceFluentContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Models.FluentAPI
+{
+    public class eCommerceFluentContext : DbContext
+    {
+        public eCommerceFluentContext(DbContextOptions<eCommerceFluentContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario>? Usuarios { get; set; }
+        public DbSet<Contato>? Contatos { get; set; }
+        public DbSet<EnderecoEntrega>? EnderecosEntrega { get; set; }
+        public DbSet<Departamento>? Departamentos { get; set; }
+
+    }
+}
