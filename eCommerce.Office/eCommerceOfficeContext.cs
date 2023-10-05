6210ffdfe2d598337776c8dcb265afb1ed6ee2c0
Matheus Moizeis <matheusmoizeis@outlook.com>
2023-10-05 07:37:37 -0300

    Add novo context do novo projeto.

--- eCommerce.Office/eCommerceOfficeContext.cs
+++ eCommerce.Office/eCommerceOfficeContext.cs
@@ -1,6 +1,18 @@
-﻿namespace eCommerce.Office
+﻿using eCommerce.Office.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Office
 {
-    public class eCommerceOfficeContext
+    public class eCommerceOfficeContext : DbContext
     {
+        public DbSet<Colaborador>? Colaboradores { get; set; }
+        public DbSet<Setor>? Setores { get; set; }
+        public DbSet<Turma>? Turmas { get; set; }
+        public DbSet<Veiculo>? Veiculos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=eCommerceOffice;Trusted_Connection=True;");
+        }
     }
 }
